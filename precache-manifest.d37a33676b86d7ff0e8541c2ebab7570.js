02da105ea866347be42e93803266d890a91191c8
t-pankajkumar <tpksym@gmail.com>
2019-10-16 15:23:28 +0530

    Updates

---
+++ precache-manifest.d37a33676b86d7ff0e8541c2ebab7570.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f167cefa70187e7cb49f81c639d4e112",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6495cd9f093dbadf2a9",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "aad94fda41d1179c44f2",
+    "url": "/static/js/2.ecc85f4f.chunk.js"
+  },
+  {
+    "revision": "e6495cd9f093dbadf2a9",
+    "url": "/static/js/main.d61945de.chunk.js"
+  },
+  {
+    "revision": "d98458e4a9194f55e15c",
+    "url": "/static/js/runtime-main.3ac33210.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/t-pankajkumar.github.io/precache-manifest.7c8eae80adc87724dcd7fc0b23ef8b16.js"
+  "/precache-manifest.d37a33676b86d7ff0e8541c2ebab7570.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/t-pankajkumar.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
